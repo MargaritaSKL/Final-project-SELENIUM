9a31baba60d9879890d3491d9a95fb49401ef880
Margarita Sokol <golovnina.margarita@yandex.ru>
2024-03-06 16:04:53 +0300

    added task 4_3 from step 68

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class ProductPageLocators: # уникальные селекторы
     BOOK_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
     BOOK_NAME_FOR_CHECK = (By.CSS_SELECTOR, "#messages > .alert-success:nth-child(1) > .alertinner strong")
     BOOK_PRICE_FOR_CHECK = (By.CSS_SELECTOR, "#messages > .alert-info > .alertinner p:nth-child(1) strong")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
